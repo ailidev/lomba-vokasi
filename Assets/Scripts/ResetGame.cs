327e6b999dd8355afc630e7a60d278cd06dc52fa
Bayu Satiyo <itsyuukunz@gmail.com>
2022-05-13 14:33:27 -0700

    Initial release
    Signed-off-by: Bayu Satiyo <itsyuukunz@gmail.com>

--- Assets/Scripts/ResetGame.cs
+++ Assets/Scripts/ResetGame.cs
@@ -1,11 +1,27 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class ResetGame : MonoBehaviour
 {
     [SerializeField] public bool m_ResetGame;
+    GameObject[] m_Player, m_Script;
 
     void Awake()
     {
+
+        if (GameManager.Instance.m_GameData.m_FirstTimePlaying)
+        {
+            m_ResetGame = true;
+        }
+        else
+        {
+            m_ResetGame = false;
+        }
+
+        m_Player = GameObject.FindGameObjectsWithTag("Player");
+        m_Script = GameObject.FindGameObjectsWithTag("Script");
+
         if (m_ResetGame)
         {
             DestroyGame();
@@ -14,7 +30,19 @@ public class ResetGame : MonoBehaviour
 
     public void DestroyGame()
     {
-        Destroy(GameObject.FindGameObjectWithTag("Player"));
-        Destroy(GameObject.FindGameObjectWithTag("Script"));
+        foreach(GameObject obj in m_Player)
+        {
+            Destroy(obj);
+        }
+
+        foreach(GameObject obj in m_Script)
+        {
+            Destroy(obj);
+        }
+    }
+
+    public void FirstGame()
+    {
+        GameManager.Instance.m_GameData.m_FirstTimePlaying = true;
     }
 }
